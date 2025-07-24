08fa21718f20b6b0db18385e6cb8432d7b4edffd
selimcagri <selimcagri@gmail.com>
2025-07-24 16:22:25 +0200

    Restore module-style LocaNMF export

--- locanmf/LocaNMF.py
+++ locanmf/LocaNMF.py
@@ -16,6 +16,19 @@ from .video import LowRankVideo
 from .demix import HalsNMF
 from .demix import LocalizedNMF
 
+__all__ = [
+    "adaptive_fit",
+    "extract_region_metadata",
+    "factor_region_videos",
+    "rank_linesearch",
+    "init_from_low_rank_video",
+    "evaluate_fit_to_region",
+    "lambda_linesearch",
+    "hals",
+    "version",
+    "LocaNMF",
+]
+
 
 def adaptive_fit(video_mats,
                  valid_mask,
@@ -592,3 +605,4 @@ def version():
 
 # for backward-compatibility / external import
 LocaNMF = LocalizedNMF
+
